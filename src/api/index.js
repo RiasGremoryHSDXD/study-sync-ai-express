4e0ebd116933a4c4f0c1f9dea491732471c24793
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-06-18 19:38:14 +0800

    wew

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,8 @@
-// api/index.js (CommonJS because Vercel Serverless prefers it)
+const express = require("express");
+const app = express();
 
-const express = require('express')
-const { getUser } = require('../src/Services/GetUser.cjs')
-const app = express()
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get('/', (req, res) => {
-  res.send('Hello from Vercel Express API!')
-})
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.get('/api/enrollments', async (req, res) => {
-  try {
-    const enrollments = await getUser()
-    res.status(200).send(enrollments)
-  } catch (error) {
-    res.status(500).send({ error: error.message })
-  }
-})
-
-module.exports = app
-
-// api/index.js (continued)
-const serverless = require('serverless-http')
-module.exports.handler = serverless(app)
+module.exports = app;
