04fb7ecc34a17e31d0a69a3240ab7508b00cc4f0
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-06-19 15:57:43 +0800

    Try Fix error 8

---
+++ api/index.mjs
@@ -0,0 +1,26 @@
+import express, { response } from 'express'
+import { getUser } from '../services/GetUser.mjs'
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello Fuck')
+})
+
+app.get('/api/enrollments', async (resquest, response) => {
+    try
+    {
+        const enrollments = await getUser();
+        response.status(200).send(enrollments)
+    }
+    catch(error){
+        response.status(500).send({error: error.message})
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on PORT ${PORT}`)
+})
+
---
+++ services/GetUser.mjs
@@ -0,0 +1,13 @@
+import { supabase } from "./supabaseClient.mjs";
+
+export async function getUser() {
+
+    const { data, error } = await supabase.rpc('user_function')
+
+    if(error){
+        console.log("RPC user_function_failed", error)
+        throw error
+    }
+
+    return data
+}
